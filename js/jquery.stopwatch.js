3bc9ce2863747fe703cb2937e8f382e3bd2a954a
Dennis Burke <dennisburke@prodigy.net>
2012-01-25 17:25:16 -0500

    Adding countdown functionality.

--- js/jquery.stopwatch.js
+++ js/jquery.stopwatch.js
@@ -6,64 +6,68 @@
 		clock.addClass('stopwatch');
 		
 		// This is bit messy, but IE is a crybaby and must be coddled. 
-		clock.html('<div class="display"><span class="hr">00</span>:<span class="min">00</span>:<span class="sec">00</span></div>');
+		clock.html('<div class="display"><span class="sec">0</span></div>');
 		clock.append('<input type="button" class="start" value="Start" />');
 		clock.append('<input type="button" class="stop" value="Stop" />');
+		clock.append('<input type="button" class="reverse" value="Reverse" />');
 		clock.append('<input type="button" class="reset" value="Reset" />');
+		clock.append('<h2 class="log"></h2>');
 		
 		// We have to do some searching, so we'll do it here, so we only have to do it once.
-		var h = clock.first('.hr');
-		var m = clock.first('.min');
-		var s = clock.first('.sec');
-		var start = clock.first('.start');
-		var stop = clock.first('.stop');
-		var reset = clock.first('.reset');
+		var s = clock.find('.sec').first();
+		var start = clock.find('.start').first();
+		var stop = clock.find('.stop').first();
+		var reset = clock.find('.reset').first();
+		var reverse = clock.find('.reverse').first();
+		var log = clock.find('.log').first();
 		
-		stop.hide();
-
+		var logcount = 1;
+		
 		start.bind('click', function() {
-			timer = setInterval(do_time, 1000);
-			stop.show();
-			start.hide();
+			timer = setInterval(countup, 1000);
 		});
 		
 		stop.bind('click', function() {
 			clearInterval(timer);
 			timer = 0;
-			start.show();
-			stop.hide();
 		});
 		
 		reset.bind('click', function() {
 			clearInterval(timer);
 			timer = 0;
-			h.html("00");
-			m.html("00");
-			s.html("00");
-			stop.hide();
-			start.show();
+			s.html('0');
+			log.empty();
+			logcount = 1;
 		});
 		
-		function do_time() {
+		reverse.bind('click', function() {
+			clearInterval(timer);
+			log.text(logcount);
+			logcount++;
+			timer = setInterval(countdown, 1000);
+		});
+		
+		function countup() {
 			// parseInt() doesn't work here...
-			hour = parseFloat(h.text());
-			minute = parseFloat(m.text());
 			second = parseFloat(s.text());
 			
 			second++;
 			
-			if(second > 59) {
-				second = 0;
-				minute = minute + 1;
-			}
-			if(minute > 59) {
-				minute = 0;
-				hour = hour + 1;
-			}
+			s.html(second);
+		}
+		
+		function countdown() {
+			// parseInt() doesn't work here...
+			second = parseFloat(s.text());
 			
-			h.html("0".substring(hour >= 10) + hour);
-			m.html("0".substring(minute >= 10) + minute);
-			s.html("0".substring(second >= 10) + second);
+			second--;
+			
+			s.html(second);
+			
+			if (second <= 0) {
+				clearInterval(timer);
+				return start.click();
+			}
 		}
 	}
 })(jQuery);
